75ea708b38d7dc27f9d7e5e71b360975edf7e291
Darío Quiñones <daqucr2@gmail.com>
2022-10-11 22:18:57 -0500

    Re-written Triangle Rendering

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::time::{SystemTime, Duration};
-// use std::vec;
 
-// use rand::Rng;
+extern crate nalgebra as na;
+// use na::Transform3;
+use na::{Vector3, Matrix4};//, U3, U4, DefaultAllocator, allocator::Allocator};
 
-mod structures;
-use structures::{Vec3, Tri, Mesh, Matrix4x4};
+mod utilities;
+use utilities::{Mesh, matrices};
 
 use sdl2;
 use sdl2::{
@@ -19,44 +20,30 @@ use sdl2::{
 // Window size
 const WIDTH: usize = 800;
 const HEIGHT: usize = 600;
-
 const FPS_CAP: u64 = 60;
-
+const BG_COLOR: Color = Color::RGB(15, 17, 17);
+const TITLE: &str = "Rust 3D Renderer";
 
 /// Application entry point
 pub fn main() {
     // Create a new window
     let (mut canvas, mut event_pump) = init_sdl();
-
     let mut time_of_last_frame = SystemTime::now();
 
-    // // RNG
-    // let mut rng = rand::thread_rng();
-
-    let z_near = 0.1; // Near clipping plane
-    let z_far = 1000.0; // Far clipping plane
-    let a = WIDTH as f32/HEIGHT as f32; // Aspect ratio
-    let fov = 60.0; // Field of view
-    let fov = 1.0 / (fov * 0.5 / 180.0 * std::f32::consts::PI).tan();
-
+    // Load the mesh
     let model_mesh = Mesh::load_from_file("assets/teapot-trian.obj");
-    let mut triangles_to_raster = Mesh::new(Vec::new());
 
-    // Projection matrix
-    let mut proj_matrix: Matrix4x4;
+    // Matrices
+    let aspect_ratio = WIDTH as f32/HEIGHT as f32; // Aspect ratio
+    let proj_matrix = matrices::projection_matrix(60.0, aspect_ratio, 0.1, 1000.0);
+    let translation_matrix = matrices::translation_matrix(0.0, 0.0, 6.0);
+    let mut world_matrix: Matrix4<f32>;
+    let camera = Vector3::new(0.0, 0.0, 0.0);
+    let mut theta:f32 = 0.0; // Rotations
 
-    // Rotation matrix
-    let mut theta:f32 = 0.0;
-    let mut rot_matrix_z: Matrix4x4;
-    let mut rot_matrix_x: Matrix4x4;
-    let mut rot_matrix: Matrix4x4;
-
-    let mut light_dir = Vec3::from([-1.0, -1.0, -1.0]);
-    light_dir.normalize();
-
-    let translation = Vec3::from([0.0, 0.0, 8.0]);
-    let camera = Vec3::from([0.0, 0.0, 0.0]);
-
+    // Lighting
+    let mut light_dir = Vector3::new(-1.0, -1.0, 0.0);
+    light_dir.normalize_mut();
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -68,63 +55,49 @@ pub fn main() {
             }
         }
 
-        canvas.set_draw_color(Color::RGB(30, 34, 34));
-        // canvas.set_draw_color(Color::RGB(0, 0, 0));
+        // Clear the screen
+        canvas.set_draw_color(BG_COLOR);
         canvas.clear();
 
         // Update Matrices
-        proj_matrix = Matrix4x4 {
-            m: [
-                [fov/a, 0.0, 0.0, 0.0],
-                [0.0, fov, 0.0, 0.0],
-                [0.0, 0.0, z_far/(z_far-z_near), 1.0],
-                [0.0, 0.0, (-z_far*z_near)/(z_far-z_near), 0.0]
-            ]
-        };
-
-        // Ripped from Wikipedia
         theta += 0.015;
-        rot_matrix_z = Matrix4x4 { m:[
-            [theta.cos(), theta.sin(), 0.0, 0.0],
-            [-theta.sin(), theta.cos(), 0.0, 0.0],
-            [0.0, 0.0, 1.0, 0.0],
-            [0.0, 0.0, 0.0, 1.0]
-        ]};
-        rot_matrix_x = Matrix4x4 { m:[
-            [1.0, 0.0, 0.0, 0.0],
-            [0.0, theta.cos(), theta.sin(), 0.0],
-            [0.0, -theta.sin(), theta.cos(), 0.0],
-            [0.0, 0.0, 0.0, 1.0]
-        ]};
+        let rot_matrix_z = matrices::rotation_matrix_z(theta);
+        let rot_matrix_x = matrices::rotation_matrix_x(theta);
 
-        // Multiply rotation matrices
-        rot_matrix = rot_matrix_z * rot_matrix_x;
-
+        // Calculate world matrix
+        world_matrix = rot_matrix_z * rot_matrix_x;
+        world_matrix = translation_matrix * world_matrix;
 
         // Draw Triangles
-        let mut rotated: Tri;
-        let mut translated: Tri;
-        let mut projected: Tri;
-        let mut normal: Vec3;
-        let mut vec_to_camera: Vec3;
+        let mut triangles_to_raster = Mesh::new(Vec::new());
         for triangle in model_mesh.tris.iter() {
-            // Rotate
-            rotated = *triangle * rot_matrix;
+            // transform triangle
+            let mut transformed = triangle.clone();
+            transformed *= world_matrix;
 
-            // Translate
-            translated = rotated + translation;
+            // Translate triangle
+            // transformed += translation;
 
             // Calculate normal
-            normal = translated.normal();
+            let normal = transformed.normal();
             // Calculate vector from camera to triangle
-            vec_to_camera = translated.p[0] - camera;
-            if normal.dot(&vec_to_camera) < 0.0 {
-                // Light
-                // Calculate light intensity
+            let mut vec_to_camera = camera - transformed.p[0].xyz();
+            vec_to_camera.normalize_mut();
+            if normal.dot(&vec_to_camera) > 0.0 {
+                // Light and Color
                 let light_intensity = normal.dot(&light_dir) + 1.0;
+                transformed.c = Option::Some(Color::RGB(
+                    (light_intensity * 127.0) as u8,
+                    (light_intensity * 127.0) as u8,
+                    (light_intensity * 127.0) as u8
+                ));
 
                 // Project triangles from 3D to 2D
-                projected = translated * proj_matrix;
+                let mut projected = proj_matrix.transpose() * transformed;
+                // Normalize the projected triangle
+                projected.p[0] /= projected.p[0].w;
+                projected.p[1] /= projected.p[1].w;
+                projected.p[2] /= projected.p[2].w;
 
                 // Scale into view
                 for i in 0..3 {
@@ -134,39 +107,42 @@ pub fn main() {
                     projected.p[i].y *= 0.5 * HEIGHT as f32;
                 }
 
-                projected.c = Option::Some(Color::RGB(
-                    (light_intensity * 127.0) as u8,
-                    (light_intensity * 127.0) as u8,
-                    (light_intensity * 127.0) as u8
-                ));
-
                 // Store triangle for rastering later
                 triangles_to_raster.tris.push(projected);
             }
         }
 
-        // Sort triangles from back to front
-        triangles_to_raster.sort();
         // Draw triangles to screen
-        for triangle in triangles_to_raster.tris.iter() {
-            triangle.draw_filled(&mut canvas);
-            // triangle.draw(&mut canvas, triangle.c.unwrap());
-        }
-
-        // Clear triangles
+        triangles_to_raster.sort();
+        draw_mesh(&triangles_to_raster, &mut canvas);
         triangles_to_raster.tris.clear();
 
+        // Update the screen
         canvas.present();
 
         // Cap FPS
-        let time_since_last_frame = SystemTime::now().duration_since(time_of_last_frame).unwrap();
-        if time_since_last_frame < Duration::from_millis(1000 / FPS_CAP) {
-            std::thread::sleep(Duration::from_millis(1000 / FPS_CAP) - time_since_last_frame);
-        }
-        time_of_last_frame = SystemTime::now();
+        limit_fps(&mut time_of_last_frame, FPS_CAP);
     }
 }
 
+fn draw_mesh(mesh: &Mesh, canvas: &mut Canvas<Window>) {
+    for triangle in mesh.tris.iter() {
+        // // Draw half the screen with draw, the other half with draw_gfx
+        // if triangle.midpoint().x < WIDTH as f32/2.0 {
+        //     triangle.draw_gfx(canvas);
+        // } else {
+            triangle.draw(canvas);
+        // }
+    }
+}
+
+fn limit_fps(prev_time: &mut SystemTime, fps_cap: u64) {
+    let time_difference = SystemTime::now().duration_since(*prev_time).unwrap();
+    if time_difference < Duration::from_millis(1000 / fps_cap) {
+        std::thread::sleep(Duration::from_millis(1000 / fps_cap) - time_difference);
+    }
+    *prev_time = SystemTime::now();
+}
 
 /// Initialize SDL
 fn init_sdl() -> (Canvas<Window>, EventPump) {
@@ -174,7 +150,7 @@ fn init_sdl() -> (Canvas<Window>, EventPump) {
     let video_subsystem = sdl_context.video().unwrap();
 
     let window = video_subsystem
-        .window("Gráficos Chingones", WIDTH as u32, HEIGHT as u32)
+        .window(TITLE, WIDTH as u32, HEIGHT as u32)
         .position_centered()
         .build()
         .unwrap();
